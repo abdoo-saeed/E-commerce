e28f7218b18a553a9b788e189c18681afdd45d81
abdo-saeed <sboda1267@gmail.com>
2025-09-30 00:43:04 +0300

    ...

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,24 @@
-import { getToken } from 'next-auth/jwt'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
-    const token = await getToken({req:request})
-    const { pathname } = request.nextUrl
-   
-    if (token && pathname.startsWith("/Auth/login")||token && pathname.startsWith("/Auth/register")) {
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request })
+  const { pathname } = request.nextUrl
+
+ 
+  if (token && (pathname.startsWith("/Auth/login") || pathname.startsWith("/Auth/register"))) {
     return NextResponse.redirect(new URL("/", request.url))
   }
 
-  
+
   if (!token && pathname.startsWith("/cart")) {
     return NextResponse.redirect(new URL("/Auth/login", request.url))
   }
 
   return NextResponse.next()
 }
- 
-// See "Matching Paths" below to learn more
+
 export const config = {
-  matcher: ["/cart","/Auth/login","/Auth/register"]
+  matcher: ["/cart", "/Auth/login", "/Auth/register"],
 }
